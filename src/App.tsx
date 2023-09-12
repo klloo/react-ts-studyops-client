56907629da956bb59f6d945e8c6e581bba8d7851
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-12 22:38:33 +0900

    feat: 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import Main from 'pages/Main';
 import React from 'react';
 import GlobalStyle from 'styles/globalStyle';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Main from 'pages/Main';
+import StudyDetail from 'pages/StudyDetail';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/study/:studyId" element={<StudyDetail />} />
+          {/* 이 경로가 없을 때 "/"로 리다이렉트된다 */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
